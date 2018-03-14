aafbaf206d73b92a8dfc9c6758e26b3efffe1943
clibing <458914534@qq.com>
2018-03-14 01:00:15 +0000

    add shell

---
+++ ubuntu.vps.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+# env
+INSTALL_DIR="/data/"
+NGINX_VERSION="1.13.9-alpine"
+DOMAINS="-d "
+
+# joint domain for acme param
+function jointDomain(){
+  DOMAINS="-d "
+  if [ "$#" -gt 1 ];then
+    # fix me
+    DOMAINS="${DOMAINS}`echo -e "$*" | sed 's/ / -d /g'`"
+  else
+    DOMAINS="-d *.$1 -d www.$1 -d $1"
+  fi
+}
+
+# init utc
+function envInit(){
+    echo -e "start env init..."
+    sudo apt-get update
+    sudo apt-get install -y lrzsz curl git openssl
+    sudo cp -rf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime 
+    sudo mkdir -p /data/ssl 
+    cd ${INSTALL_DIR}
+    if [ ! -d "${INSTALL_DIR}/install" ];then
+        git clone https://github.com/clibing/vps-install.git install
+    else
+        cd ${INSTALL_DIR}/install
+        git pull
+    fi
+}
+
+# install docker ce
+function dockerInstall(){
+    sudo apt-get remove docker docker-engine docker.io
+    sudo apt-get update
+    sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo apt-key fingerprint 0EBFCD88 
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" 
+    sudo apt-get update
+    sudo apt-get install docker-ce -y
+    sudo docker run --rm hello-world
+}
+
+# install nginx for verification acme
+function installNginx(){
+   echo -e "start install nginx domain: [$1]"
+   ngxconf="/data/install/nginx/conf/nginx-`date +%Y%m%d%H%M%S`.conf"
+   cp /data/install/nginx/conf/nginx.conf ${ngxconf}
+   sed "s/DOMAIN_REPLACE/$1/g" -i ${ngxconf}
+   docker run -d -v ${ngxconf}:/etc/nginx/nginx.conf -v /data/install/nginx/html:/usr/share/nginx/html --name "acme-nginx" -p 80:80 nginx:${NGINX_VERSION}
+   # run by docker 
+   # must set ssl file and open 443, will the file share to github.com
+}
+
+# install let's encrypt
+function installLetSencrypt(){
+    echo -e "start install let's encrypt"
+    sudo curl https://get.acme.sh | sh
+    sudo source ~/.bashrc 
+    echo -e "`acme.sh --issue $1 -w /data/install/nginx/html`"
+    # cp ssl ca to nginx conf/ssl
+    acme.sh --installcert -d $1 \
+            --keypath       /data/ssl/$1.key  \
+            --fullchainpath /data/ssl/$1.key.pem 
+    openssl dhparam -out /data/ssl/dhparam.pem 2048
+    docker stop acme-nginx
+}
+
+# set www.aliyun.com dns
+function updateDnsRecord(){
+   domain="${1}"
+   # only lo and eth0
+   address="`ip address | grep 'inet' | grep -v 'inet6' | grep -v '127.0.0.1'|awk '{print $2}' | awk -F '/' '{print $1}'`"
+   # update dns record by api
+}
+
+# update nginx for ssl
+function updateNginx(){
+   echo -e "start install nginx will set domain: $1"
+   # run by docker 
+   # must set ssl file and open 443, will the file share to github.com
+   ngxconf="/data/install/nginx/conf/nginx-`date +%Y%m%d%H%M%S`.conf"
+   cp /data/install/nginx/conf/nginx.conf ${ngxconf} 
+   sed 's/#ssl_protocols/ssl_protocols/g' -i ${ngxconf}
+   sed 's/#ssl_prefer_server_ciphers/ssl_prefer_server_ciphers/g' -i ${ngxconf}
+   sed "s/DOMAIN_REPLACE/$1/g" -i ${ngxconf}
+   sed 's/#ssl_certificate/ssl_certificate/g' -i ${ngxconf}
+   sed 's/#ssl_certificate_key/ssl_certificate_key/g' -i ${ngxconf} 
+   sed 's/#ssl_dhparam/ssl_dhparam/g' -i ${ngxconf}
+   
+   # stop nginx-docker
+   docker rm acme-nginx
+   docker run -d -v ${ngxconf}:/etc/nginx/nginx.conf -v /data/install/nginx/html:/usr/share/nginx/html -v /data/ssl:/etc/nginx/ssl --name "acme-nginx" -p 80:80 nginx:${NGINX_VERSION}
+}
+
+domain="linuxcrypt.top"
+
+# init env
+#envInit
+
+# install docker ce
+# dockerInstall
+
+# add domain
+#jointDomain ${domain}
+
+# add http for domain
+#installNginx ${domain}
+
+# create ssl ca
+#installLetSencrypt ${DOMAINS}
+
+# create nginx-.conf from nginx-template.conf
+updateNginx ${domain}
+
+
+
